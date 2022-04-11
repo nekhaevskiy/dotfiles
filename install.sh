3cb47c843b65ece931987e942631686805adb3ad
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2022-04-11 09:31:20 +0200

    Add i3 config

--- install.sh
+++ install.sh
@@ -11,3 +11,10 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+
+######
+# i3 #
+######
+
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
