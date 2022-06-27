a736379134f36cbba9f1e980b95ca577d899d2c2
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2022-06-27 11:09:05 +0200

    X11: update office monitor setup

--- X11/monitor-office.sh
+++ X11/monitor-office.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-3 --off
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DP-3 --off
