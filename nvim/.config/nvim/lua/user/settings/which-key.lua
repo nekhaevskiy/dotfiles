bd4de2c0479e2c2d188575beaa20669bc3956f7e
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2022-10-06 10:52:36 +0200

    nvim: update keybindings

--- nvim/.config/nvim/lua/user/settings/which-key.lua
+++ nvim/.config/nvim/lua/user/settings/which-key.lua
@@ -5,23 +5,30 @@ end
 
 local mappings = {
   ["/"] = { "<cmd>nohlsearch<cr>", "No highlight" },
-  ["b"] = { "<cmd>lua require('telescope.builtin').buffers({previewer=false})<cr>", "Buffers", },
   ["e"] = { "<cmd>NvimTreeFindFileToggle<cr>", "Explorer" },
   ["j"] = { "<cmd>lua require 'hop'.hint_words()<cr>", "Jump to word" },
   ["p"] = { "<cmd>PackerSync<cr>", "Packer sync" },
-  ["q"] = { "<cmd>q<cr>", "Close buffer" },
-  ["w"] = { "<cmd>w<cr>", "Write buffer"},
+
+  d = {
+    name = "Diagnostics",
+    d = { "<cmd>lua require('telescope.builtin').diagnostics()<cr>", "All" },
+    j = { "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", "Next", },
+    k = { "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>", "Prev" },
+    l = { "<cmd>lua vim.diagnostic.open_float()<cr>", "Line" },
+    q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
+  },
 
   f = {
     name = "Find",
+    b = { "<cmd>lua require('telescope.builtin').buffers({previewer=false})<cr>", "Buffers", },
     c = { "<cmd>Telescope commands<cr>", "Commands" },
     f = { "<cmd>lua require('telescope.builtin').find_files({previewer=false})<cr>", "Files", },
     g = { "<cmd>lua require('telescope.builtin').live_grep()<cr>", "Grep" },
     h = { "<cmd>Telescope help_tags<cr>", "Help" },
     k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
     m = { "<cmd>Telescope man_pages<cr>", "Man pages" },
+    o = { "<cmd>lua require('telescope.builtin').oldfiles({previewer=false})<cr>", "Recent files" },
     r = { "<cmd>Telescope registers<cr>", "Registers" },
-    R = { "<cmd>lua require('telescope.builtin').oldfiles({previewer=false})<cr>", "Recent files" },
   },
 
   g = {
@@ -41,18 +48,14 @@ local mappings = {
   l = {
     name = "LSP",
     a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code action" },
-    d = { "<cmd>lua require('telescope.builtin').diagnostics()<cr>", "Diagnostics" },
-    f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" },
+    d = { "<cmd>lua require('telescope.builtin').lsp_definitions({jump_type='never'})<cr>", "Definitions" },
+    f = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format" },
     i = { "<cmd>LspInfo<cr>", "Info" },
     I = { "<cmd>LspInstallInfo<cr>", "Installer info" },
-    j = { "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", "Next diagnostic", },
-    k = { "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>", "Prev diagnostic" },
-    l = { "<cmd>lua vim.diagnostic.open_float()<cr>", "Line diagnostics" },
-    q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
-    R = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
     r = { "<cmd>lua require('telescope.builtin').lsp_references({show_line=false})<cr>", "References" },
-    s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document symbols" },
+    R = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
     S = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace symbols" },
+    s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document symbols" },
   },
 }
 
