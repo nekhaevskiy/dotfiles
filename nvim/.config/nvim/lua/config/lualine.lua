e3e3bbac441e1c7d8f60598e38587fc4eded3f6d
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2022-10-07 15:14:22 +0200

    nvim: move lualine config to a separate file

---
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -0,0 +1,13 @@
+local lualine = require('lualine')
+
+lualine.setup({
+  options = { theme = 'onedark' },
+  tabline = {
+    lualine_a = { 'buffers' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'tabs' }
+  }
+})
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -117,19 +117,7 @@ return packer.startup(function(use)
   use {
     "nvim-lualine/lualine.nvim",
     requires = { "kyazdani42/nvim-web-devicons", opt = true },
-    config = function()
-      require('lualine').setup({
-        options = { theme = 'onedark' },
-        tabline = {
-          lualine_a = { 'buffers' },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { 'tabs' }
-        }
-      })
-    end
+    config = [[require('config.lualine')]]
   }
 
   -- surrounding
