3827326ee991af7da3b1cf0de412e46192a7241d
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2022-10-12 06:45:07 +0200

    nvim: update lualine config

--- nvim/.config/nvim/lua/config/lualine.lua
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -1,13 +1,20 @@
 local lualine = require('lualine')
 
 lualine.setup({
-  options = { theme = 'onedark' },
-  -- tabline = {
-  --   lualine_a = { 'buffers' },
-  --   lualine_b = {},
-  --   lualine_c = {},
-  --   lualine_x = {},
-  --   lualine_y = {},
-  --   lualine_z = { 'tabs' }
-  -- }
+  options = {
+    theme = 'onedark',
+    -- globalstatus = true
+  },
+  -- sections = {
+  --   lualine_b = { 'filename' },
+  --   lualine_c = { 'branch', 'diff', 'diagnostics' },
+  -- },
+  tabline = {
+    lualine_a = { 'buffers' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'tabs' }
+  }
 })
