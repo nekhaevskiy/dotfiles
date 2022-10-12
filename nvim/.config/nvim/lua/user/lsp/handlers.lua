aa9571d89f0705f89a4729ee6f44784d0f5e24cf
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2022-10-12 06:39:47 +0200

    nvim: update keybindings

--- nvim/.config/nvim/lua/user/lsp/handlers.lua
+++ nvim/.config/nvim/lua/user/lsp/handlers.lua
@@ -60,19 +60,11 @@ local function lsp_highlight_document(client)
   end
 end
 
-local function lsp_keymaps(bufnr)
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-end
 
-M.on_attach = function(client, bufnr)
+M.on_attach = function(client)
   if client.name == "tsserver" then
     client.server_capabilities.document_formatting = false
   end
-  lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
 
