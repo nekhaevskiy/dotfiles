70f9c1ff4c133aba798b17810eb189d9635e81fc
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2022-11-09 08:49:54 +0100

    nvim: update options

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,23 +1,32 @@
 -- :help options
-vim.opt.fileencoding = "utf-8"
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.title = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.inccommand = "split"
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.smartindent = true
+
+-- vim.opt.wrap = false
+
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.smartindent = true
-vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
-vim.opt.wildignore:append({ "*/node_modules/*" })
+
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
+-- vim.opt.fileencoding = "utf-8"
+-- vim.opt.title = true
+-- vim.opt.scrolloff = 10
+-- vim.opt.inccommand = "split"
+-- vim.opt.breakindent = true
+-- vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+-- vim.opt.wildignore:append({ "*/node_modules/*" })
 
 -- highlight configuration
 vim.opt.cursorline = true
