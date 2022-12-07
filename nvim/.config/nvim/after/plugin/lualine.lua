f12930ed844ce7906f69ff726f96cf244e1b866f
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2022-12-07 07:07:01 +0100

    nvim: update lualine config

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -1,19 +1,25 @@
-require("lualine").setup({
+local lualine = require("lualine")
+
+lualine.setup({
 	options = {
 		theme = "onedark",
+		globalstatus = true,
 	},
 	sections = {
-		lualine_b = { "filename" },
-		lualine_c = { "branch" },
-		lualine_x = {
+		lualine_a = { "mode" },
+		lualine_b = {
+			"branch",
+			"diff",
 			{
 				"diagnostics",
 				sources = { "nvim_diagnostic" },
 				symbols = { error = " ", warn = " ", info = " ", hint = " " },
 			},
-			"encoding",
-			"filetype",
 		},
+		lualine_c = { "filename" },
+		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
 	},
 	inactive_sections = {
 		lualine_b = {
@@ -26,6 +32,12 @@ require("lualine").setup({
 		lualine_c = {},
 	},
 	tabline = {
-		lualine_a = { { "tabs", max_length = vim.o.columns, mode = 2 } },
+		lualine_a = { { "tabs", max_length = vim.o.columns, mode = 0 } },
+	},
+	winbar = {
+		lualine_c = { "filename" },
+	},
+	inactive_winbar = {
+		lualine_c = { "filename" },
 	},
 })
