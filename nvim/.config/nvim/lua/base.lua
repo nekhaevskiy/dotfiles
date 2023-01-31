16d692a89c9dc22cbf29d2193d5c6aec90b9774e
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2023-01-31 15:12:16 +0100

    nvim: fix undodir option

--- nvim/.config/nvim/lua/base.lua
+++ nvim/.config/nvim/lua/base.lua
@@ -8,7 +8,7 @@ vim.opt.swapfile = false
 
 -- save undo-trees in files
 vim.opt.undofile = true
-vim.opt.undodir = "$HOME/.config/nvim/undo//"
+vim.opt.undodir = os.getenv("XDG_CONFIG_HOME") .. "/nvim/undo"
 
 -- number of undo saved
 vim.opt.undolevels = 10000
