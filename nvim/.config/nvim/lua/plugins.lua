d949d687020f3cdce74d36ddf5576edbc48e10b1
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2023-02-01 09:35:34 +0100

    nvim: automatically install and set up packer.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,3 +1,16 @@
+-- automatically install and set up packer.nvim
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+local packer_bootstrap = ensure_packer()
+
 -- only required if you have packer configured as `opt`
 vim.cmd([[packadd packer.nvim]])
 
@@ -66,4 +79,8 @@ return require("packer").startup(function(use)
 
 	use("tpope/vim-surround")
 	use("christoomey/vim-tmux-navigator")
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
