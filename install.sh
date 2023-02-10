4d47607c9c29e9cf9188c484e98b3e51709772d9
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2023-02-10 18:10:44 +0100

    install.sh: update links with env variables

--- install.sh
+++ install.sh
@@ -4,36 +4,36 @@
 # nvim #
 ########
 
-# mkdir -p "$HOME/.config/nvim"
-# mkdir -p "$HOME/.config/nvim/undo"
+# mkdir -p "$XDG_CONFIG_HOME/nvim"
+# mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
-# ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
+# ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
 
 #######
 # X11 #
 #######
 
-rm -rf "$HOME/.config/X11"
-ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/X11"
+ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
 ######
 # i3 #
 ######
 
-rm -rf "$HOME/.config/i3"
-ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/i3"
+ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
 #######
 # zsh #
 #######
 
 rm "$HOME/.zshenv"
-rm -rf "$HOME/.config/zsh"
-mkdir -p "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
-ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
+rm -rf "$XDG_CONFIG_HOME/zsh"
+mkdir -p "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
+ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
 #########
 # fonts #
@@ -46,12 +46,12 @@ cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 # alacritty #
 #############
 
-rm -rf "$HOME/.config/alacritty"
-ln -s "$HOME/dotfiles/alacritty" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/alacritty"
+ln -s "$DOTFILES/alacritty" "$XDG_CONFIG_HOME"
 
 ###########
 # zathura #
 ###########
 
-rm -rf "$HOME/.config/zathura"
-ln -s "$HOME/dotfiles/zathura" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/zathura"
+ln -s "$DOTFILES/zathura" "$XDG_CONFIG_HOME"
