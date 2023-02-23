22912ababb997ff86fc2c975f29211ae33f777e4
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2023-02-23 05:35:15 +0100

    nvim: change config to LazyVim

--- nvim/after/plugin/defaults.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.relativenumber = true
-
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add onedark
+  { "navarasu/onedark.nvim" },
+
+  -- Configure LazyVim to load onedark
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
