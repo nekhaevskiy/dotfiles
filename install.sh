a3f440728f7dc4616a4780332aed5c1e7ed62e70
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2023-02-25 08:07:47 +0100

    install.sh: reorder alphabetically

--- install.sh
+++ install.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 
+#############
+# alacritty #
+#############
+
+rm -rf "$XDG_CONFIG_HOME/alacritty"
+ln -s "$DOTFILES/alacritty" "$XDG_CONFIG_HOME"
+
+#########
+# dunst #
+#########
+
+rm -rf "$XDG_CONFIG_HOME/dunst"
+ln -s "$DOTFILES/dunst" "$XDG_CONFIG_HOME"
+
+#########
+# fonts #
+#########
+
+mkdir -p "$XDG_DATA_HOME"
+cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+
+#######
+# git #
+#######
+
+rm -rf "$XDG_CONFIG_HOME/git"
+ln -s "$DOTFILES/git" "$XDG_CONFIG_HOME"
+
+######
+# i3 #
+######
+
+rm -rf "$XDG_CONFIG_HOME/i3"
+ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
+
 ########
 # nvim #
 ########
 
-# mkdir -p "$XDG_CONFIG_HOME/nvim"
-# mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
-
-# ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
+rm -rf "$XDG_CONFIG_HOME/nvim"
+ln -s "$DOTFILES/nvim" "$XDG_CONFIG_HOME"
 
 #######
 # X11 #
@@ -16,12 +49,12 @@
 rm -rf "$XDG_CONFIG_HOME/X11"
 ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
-######
-# i3 #
-######
+###########
+# zathura #
+###########
 
-rm -rf "$XDG_CONFIG_HOME/i3"
-ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
+rm -rf "$XDG_CONFIG_HOME/zathura"
+ln -s "$DOTFILES/zathura" "$XDG_CONFIG_HOME"
 
 #######
 # zsh #
@@ -34,32 +67,3 @@ ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
 ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
-
-#########
-# fonts #
-#########
-
-mkdir -p "$XDG_DATA_HOME"
-cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
-
-#########
-# dunst #
-#########
-
-rm -rf "$XDG_CONFIG_HOME/dunst"
-mkdir -p "$XDG_CONFIG_HOME/dunst"
-ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst"
-
-#############
-# alacritty #
-#############
-
-rm -rf "$XDG_CONFIG_HOME/alacritty"
-ln -s "$DOTFILES/alacritty" "$XDG_CONFIG_HOME"
-
-###########
-# zathura #
-###########
-
-rm -rf "$XDG_CONFIG_HOME/zathura"
-ln -s "$DOTFILES/zathura" "$XDG_CONFIG_HOME"
