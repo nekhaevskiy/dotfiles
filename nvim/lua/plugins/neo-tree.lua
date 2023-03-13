b5063228f8026d2d4f6ff48b1070e2945dfed19f
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2023-03-13 17:21:28 +0100

    nvim: update split keymappings for neo-tree

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      mappings = {
+        ["S"] = "none",
+        ["_"] = "open_split",
+        ["s"] = "none",
+        ["|"] = "open_vsplit",
+      },
+    },
+  },
+}
