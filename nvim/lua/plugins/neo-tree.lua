4fdb0efad6986b89742b3341e0f36a826dd31334
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2023-03-17 18:14:38 +0100

    nvim: close neo-tree on file opening

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,15 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    event_handlers = {
+      {
+        event = "file_opened",
+        handler = function()
+          --auto close
+          require("neo-tree").close_all()
+        end,
+      },
+    },
     window = {
       mappings = {
         ["S"] = "none",
