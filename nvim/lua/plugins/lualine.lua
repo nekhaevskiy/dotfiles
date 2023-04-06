e9f4b05e22cba802949ade034091e29d9189280f
Yury Nekhaevskiy <nekhaevskiy@me.com>
2023-04-06 19:01:03 +0200

    nvim: update lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,93 +1,17 @@
 return {
   "nvim-lualine/lualine.nvim",
-  opts = function()
-    local icons = require("lazyvim.config").icons
-
-    local function fg(name)
-      return function()
-        ---@type {foreground?:number}?
-        local hl = vim.api.nvim_get_hl_by_name(name, true)
-        return hl and hl.foreground and { fg = string.format("#%06x", hl.foreground) }
-      end
-    end
-
-    return {
-      options = {
-        theme = "auto",
-        globalstatus = false,
-        disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
+  opts = {
+    winbar = {
+      lualine_c = {
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { "filename", path = 0, symbols = { modified = "  ", readonly = "", unnamed = "" } },
       },
-      sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {
-          { "filename", symbols = { modified = "  ", readonly = "", unnamed = "" } },
-          {
-            "diagnostics",
-            symbols = {
-              error = icons.diagnostics.Error,
-              warn = icons.diagnostics.Warn,
-              info = icons.diagnostics.Info,
-              hint = icons.diagnostics.Hint,
-            },
-          },
-            -- stylua: ignore
-            {
-              function() return require("nvim-navic").get_location() end,
-              cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
-            },
-        },
-        lualine_x = {
-            -- stylua: ignore
-            {
-              function() return require("noice").api.status.command.get() end,
-              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-              color = fg("Statement")
-            },
-            -- stylua: ignore
-            {
-              function() return require("noice").api.status.mode.get() end,
-              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-              color = fg("Constant") ,
-            },
-          { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = fg("Special") },
-          {
-            "diff",
-            symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
-            },
-          },
-        },
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {},
+    },
+    inactive_winbar = {
+      lualine_c = {
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { "filename", path = 0, symbols = { modified = "  ", readonly = "", unnamed = "" } },
       },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = {},
-        lualine_y = {
-          { "location", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {},
-      },
-      tabline = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = {
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
-        },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-      extensions = { "mundo", "neo-tree" },
-    }
-  end,
+    },
+  },
 }
