2bc07ba27a407c8f3162afdcaa787408ede34195
Yury Nekhaevskiy <nekhaevskiy@me.com>
2023-04-11 18:28:08 +0200

    nvim: update gd keybinding

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,5 +3,6 @@ return {
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = { "gr", "<cmd>lua require('telescope.builtin').lsp_references({ show_line = false })<cr>" }
+    keys[#keys + 1] = { "gd", "<cmd>lua require('telescope.builtin').lsp_definitions({ show_line = false })<cr>" }
   end,
 }
