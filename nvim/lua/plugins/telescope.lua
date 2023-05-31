bdc038ca552106831237351b14c6d68bbd055e5a
Yury Nekhaevskiy <nekhaevskiy@me.com>
2023-05-31 11:27:55 +0200

    nvim: update telescope keybindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-telescope/telescope.nvim",
+  keys = {
+    { "<leader>bb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+  },
   opts = {
     defaults = {
       layout_strategy = "vertical",
