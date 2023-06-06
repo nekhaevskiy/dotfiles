6aedcd2b61dbc7f8aad05ea5cedae6aebf651fdd
Yury Nekhaevskiy <nekhaevskiy@me.com>
2023-06-06 18:19:58 +0200

    nvim: add telescope-zf-native sorter for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,11 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "natecraddock/telescope-zf-native.nvim",
+    config = function()
+      require("telescope").load_extension("zf-native")
+    end,
+  },
   keys = {
     { "<leader>bb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
   },
