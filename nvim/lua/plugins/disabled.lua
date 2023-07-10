8d5cd0c78d3abe53c4e6aa0be8dc96f4bf671a78
Yury Nekhaevskiy <nekhaevskiy@me.com>
2023-07-10 14:50:49 +0200

    nvim: disable noice.nvim

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,7 +1,19 @@
 return {
-  { "goolord/alpha-nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "echasnovski/mini.bufremove", enabled = false },
-  { "folke/persistence.nvim", enabled = false },
+  -- Coding
   { "echasnovski/mini.pairs", enabled = false },
+
+  -- Colorscheme
+  { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+
+  -- Editor
+  { "echasnovski/mini.bufremove", enabled = false },
+
+  -- UI
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+
+  -- Util
+  { "folke/persistence.nvim", enabled = false },
 }
