a459b97e1a3d38aee10b638e2e3a7aea0031e654
Yury Nekhaevskiy <nekhaevskiy@me.com>
2023-07-10 15:43:54 +0200

    nvim: fix description for gd and gr keymaps

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,13 @@ return {
   "neovim/nvim-lspconfig",
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "gr", "<cmd>lua require('telescope.builtin').lsp_references({ show_line = false })<cr>" }
-    keys[#keys + 1] = { "gd", "<cmd>lua require('telescope.builtin').lsp_definitions({ show_line = false })<cr>" }
+    keys[#keys + 1] = {
+      "gd",
+      "<cmd>lua require('telescope.builtin').lsp_definitions({ show_line = false })<cr>",
+      desc = "Goto Definition",
+      has = "definition",
+    }
+    keys[#keys + 1] =
+      { "gr", "<cmd>lua require('telescope.builtin').lsp_references({ show_line = false })<cr>", desc = "References" }
   end,
 }
