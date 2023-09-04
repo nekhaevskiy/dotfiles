2b4f7fde2fc5919285d443fab772246a6e80c07f
Yura Nekhaevskiy <nekhaevskiy@me.com>
2023-09-04 16:03:51 +0200

    nvim: update telescope keybindings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,8 +1,8 @@
 local status, builtin = pcall(require, "telescope.builtin")
 if (not status) then return end
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
