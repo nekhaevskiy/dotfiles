eed90cd43e44fb16365ff8083b1e05b74dce63a8
Yura Nekhaevskiy <nekhaevskiy@me.com>
2023-09-06 14:18:52 +0200

    nvim: update harpoon keybindings

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -5,8 +5,7 @@ local status_ui, ui = pcall(require, "harpoon.ui")
 if (not status_ui) then return end
 
 vim.keymap.set("n", "<leader>ha", mark.add_file)
-vim.keymap.set("n", "<leader>ht", ui.toggle_quick_menu)
-
+vim.keymap.set("n", "<leader>hh", ui.toggle_quick_menu)
 vim.keymap.set("n", "<leader>h1", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<leader>h2", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<leader>h3", function() ui.nav_file(3) end)
