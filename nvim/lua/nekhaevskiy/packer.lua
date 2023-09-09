e35e77863356186e8aacec2db118eba0693e3b1f
Yura Nekhaevskiy <nekhaevskiy@me.com>
2023-09-09 15:07:40 +0200

    nvim: move harpoon to lazy.nvim

--- nvim/lua/nekhaevskiy/packer.lua
+++ nvim/lua/nekhaevskiy/packer.lua
@@ -12,10 +12,8 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
-
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 	use 'nvim-treesitter/playground'
-	use 'theprimeagen/harpoon'
 	use 'mbbill/undotree'
 	use 'tpope/vim-fugitive'
 
