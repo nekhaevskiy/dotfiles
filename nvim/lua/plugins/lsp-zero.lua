3338afb15453f40a08cb2a47fbc7edddc9b4e4d0
Yura Nekhaevskiy <nekhaevskiy@me.com>
2023-09-17 11:16:05 +0200

    nvim: add lsp-zero.nvim

---
+++ nvim/lua/plugins/lsp-zero.lua
@@ -0,0 +1,29 @@
+return {
+  "VonHeikemen/lsp-zero.nvim",
+  dependencies = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim'},           -- Optional
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  },
+  config = function()
+    local lsp = require('lsp-zero').preset({})
+
+    lsp.on_attach(function(client, bufnr)
+      -- see :help lsp-zero-keybindings
+      -- to learn the available actions
+      lsp.default_keymaps({buffer = bufnr})
+    end)
+
+    -- (Optional) Configure lua language server for neovim
+    require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+    lsp.setup()
+  end,
+  branch = 'v2.x',
+}
