1c59e07986dcfb0665e2b01621ff99a13a081350
Yura Nekhaevskiy <nekhaevskiy@me.com>
2023-09-19 19:12:18 +0200

    nvim: update lsp-zero to v3

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -12,18 +12,50 @@ return {
     {'L3MON4D3/LuaSnip'},     -- Required
   },
   config = function()
-    local lsp = require('lsp-zero').preset({})
+    local lsp_zero = require('lsp-zero')
 
-    lsp.on_attach(function(client, bufnr)
+    lsp_zero.on_attach(function(_, bufnr)
       -- see :help lsp-zero-keybindings
       -- to learn the available actions
-      lsp.default_keymaps({buffer = bufnr})
+      lsp_zero.default_keymaps({buffer = bufnr})
     end)
 
-    -- (Optional) Configure lua language server for neovim
-    require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      ensure_installed = {},
+      handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+          -- (Optional) configure lua language server
+          local lua_opts = lsp_zero.nvim_lua_ls()
+          require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+      },
+    })
 
-    lsp.setup()
+    ---
+    -- Autocompletion config
+    ---
+    local cmp = require('cmp')
+    local cmp_action = lsp_zero.cmp_action()
+
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- Scroll up and down in the completion documentation
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+      })
+    })
   end,
-  branch = 'v2.x',
+  branch = 'v3.x',
 }
