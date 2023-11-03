e027d7d46cddab03b6b0d7581de50dacfad6a172
Yury Nekhaevskiy <nekhaevskiy@me.com>
2023-11-03 09:49:56 +0100

    nvim: update gitsigns keybindings

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -16,8 +16,8 @@ return {
         vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
       end
 
-      map("n", "]h", gs.next_hunk, "Next Hunk")
-      map("n", "[h", gs.prev_hunk, "Prev Hunk")
+      map("n", "]g", gs.next_hunk, "Next Hunk")
+      map("n", "[g", gs.prev_hunk, "Prev Hunk")
       map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
       map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
       map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
