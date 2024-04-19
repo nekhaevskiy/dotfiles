b0115e46e0d88021a8785f6497614e822f456797
Yury Nekhaevskiy <nekhaevskiy@macbook.air>
2024-04-19 21:09:25 +0200

    nvim: update telescope keybindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,6 +6,11 @@ return {
     },
   },
   keys = {
+    {
+      "<leader>fb",
+      "<cmd>Telescope buffers sort_mru=true sort_lastused=true initial_mode=normal<cr>",
+      desc = "Buffers",
+    },
     { "<leader>gs", "<cmd>Telescope git_status initial_mode=normal<CR>", desc = "Status" },
   },
 }
