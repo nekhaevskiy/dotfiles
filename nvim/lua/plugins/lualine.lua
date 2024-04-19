4ea5ebad8bb01f0e5bafc37fbeadfed0fe90e4d2
Yury Nekhaevskiy <nekhaevskiy@macbook.air>
2024-04-19 21:09:42 +0200

    nvim: change lualine config

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,89 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function()
+    -- PERF: we don't need this lualine require madness 🤷
+    local lualine_require = require("lualine_require")
+    lualine_require.require = require
+
+    local icons = require("lazyvim.config").icons
+
+    vim.o.laststatus = vim.g.lualine_laststatus
+
+    return {
+      options = {
+        theme = "onedark",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filename", path = 1 },
+        },
+        lualine_x = {
+          { "filetype", separator = "", padding = { left = 1, right = 0 } },
+        },
+        lualine_y = {
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.command.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          color = LazyVim.ui.fg("Statement"),
+        },
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.mode.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+          color = LazyVim.ui.fg("Constant"),
+        },
+        -- stylua: ignore
+        {
+          function() return "  " .. require("dap").status() end,
+          cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
+          color = LazyVim.ui.fg("Debug"),
+        },
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = LazyVim.ui.fg("Special"),
+          },
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+        },
+        lualine_z = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+      },
+      extensions = { "neo-tree", "lazy" },
+    }
+  end,
+}
