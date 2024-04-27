bed347bc9007deeb7988161ea45c50760386172a
Yury Nekhaevskiy <nekhaevskiy@macbook.air>
2024-04-27 20:15:22 +0200

    nvim: move disable plugin to disabled.lua

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+}
