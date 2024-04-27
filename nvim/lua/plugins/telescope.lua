e2b2142c6e44003037713245b1f4f823adc4cd44
Yury Nekhaevskiy <nekhaevskiy@macbook.air>
2024-04-27 20:20:16 +0200

    nvim: add telescope-fzf-native

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,12 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
   opts = {
     defaults = {
       layout_strategy = "vertical",
