2ed955cecc117f7697ed98e8da8d5e61f076aa5a
Yury Nekhaevskiy <nekhaevskiy@dell-xps-15>
2024-06-27 13:33:27 +0200

    screenlayout: add layouts - default/home/office

---
+++ screenlayout/.screenlayout/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1200 --pos 3840x200 --rotate normal --output DP-1 --primary --mode 3840x1600 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off
