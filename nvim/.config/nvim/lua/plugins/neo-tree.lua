30dee32a2cb0415f9a1aa51f3a3bea74eaa61b3f
Yury Nekhaevskiy <nekhaevskiy@dell-xps-15>
2024-06-28 18:40:47 +0200

    nvim: show hidden files in neo-tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -8,7 +8,12 @@ return {
   },
   config = function()
     require("neo-tree").setup({
-        close_if_last_window = true
+      filesystem = {
+        filtered_items = {
+          visible = true
+        }
+      },
+      close_if_last_window = true
     })
 
     vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle current reveal_force_cwd<cr>")
