142e4db6c070224071459b45376c83cf24bde63d
Yury Nekhaevskiy <nekhaevskiy@dell-xps-15>
2024-07-05 21:25:48 +0200

    nvim: update telescope keybindings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -34,6 +34,7 @@ return {
       vim.keymap.set("n", "<leader>dl", builtin.diagnostics)
 
       vim.keymap.set("n", "<leader>ff", builtin.find_files)
+      vim.keymap.set("n", "<leader>fg", builtin.git_files)
       vim.keymap.set("n", "<leader>fh", builtin.help_tags)
       vim.keymap.set("n", "<leader>fr", builtin.oldfiles)
       vim.keymap.set("n", "<leader>fs", builtin.live_grep)
