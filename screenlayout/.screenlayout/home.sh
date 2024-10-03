98f16f8a36403950b5368863ce8a9e8736b625ff
Yury Nekhaevskiy <nekhaevskiy@dell-xps-15>
2024-10-03 09:50:12 +0200

    update screenlayouts

--- screenlayout/.screenlayout/home.sh
+++ screenlayout/.screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1200 --pos 3840x0 --rotate normal --output DP-1 --off --output DP-2 --mode 3840x1600 --pos 0x0 --rotate normal --output DP-3 --off
+xrandr --output eDP-1 --mode 1920x1200 --pos 3840x400 --rotate normal --output DP-1 --primary --mode 3840x1600 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off
