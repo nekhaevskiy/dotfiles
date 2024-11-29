c5aa40eafb637fa25212268a8f4d24dd72ed9d8c
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2024-11-29 22:55:46 +0100

    nvim: add shfmt to none-ls

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -5,7 +5,5 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { remap = true })
 
 vim.keymap.set({ "n", "i" }, "<esc>", "<cmd>noh<cr><esc>")
 
--- vim.keymap.set("n", "<leader>bb", "<cmd>e #<cr>")
---
 vim.keymap.set("n", "]q", "<cmd>cnext<cr>")
 vim.keymap.set("n", "[q", "<cmd>cprev<cr>")
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,17 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.stylua,
-      },
-    })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.shfmt,
+				null_ls.builtins.formatting.stylua,
+			},
+		})
 
-    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
-  end,
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
+	end,
 }
