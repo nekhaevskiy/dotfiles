2bb78f6909d0ffd6e8a4c4baf55c77ddf79b4ff9
Yury Nekhaevskiy <nekhaevskiy@macbook.air>
2024-12-02 10:21:54 +0100

    nvim: install kickstart.nvim

--- nvim/.config/nvim/lua/autocmds.lua
+++
@@ -1,7 +0,0 @@
--- Highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
--- nvim/.config/nvim/lua/keymaps.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set("n", "<C-h>", "<C-w>h", { remap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { remap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { remap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { remap = true })
-
-vim.keymap.set({ "n", "i" }, "<esc>", "<cmd>noh<cr><esc>")
-
-vim.keymap.set("n", "]q", "<cmd>cnext<cr>")
-vim.keymap.set("n", "[q", "<cmd>cprev<cr>")
