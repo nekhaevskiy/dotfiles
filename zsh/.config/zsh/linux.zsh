90ca9468b9d8d8a11a8fa63eeb508550bed693cd
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2024-12-04 12:33:49 +0100

    zsh: update linux config

--- zsh/.config/zsh/linux.zsh
+++ zsh/.config/zsh/linux.zsh
@@ -1,9 +1,27 @@
+###########
+# Plugins #
+###########
+
+# fzf
+source /usr/share/doc/fzf/examples/key-bindings.zsh
+source /usr/share/doc/fzf/examples/completion.zsh
+FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
+FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 # nvm
-source /usr/share/nvm/init-nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# starship
+eval "$(starship init zsh)"
+
+# zoxide
+eval "$(zoxide init zsh)"
 
 # zsh-autosuggestions
-source "/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 bindkey '^y' autosuggest-accept # use <C-y> keybinding to accept autosuggestion
 
 # zsh syntax highlighting
-source "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
