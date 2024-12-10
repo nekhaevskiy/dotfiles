0a5b2b868a9de06a6b0f4dcc7376d226c54d6adc
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2024-12-10 13:34:57 +0100

    nvim: update telescope keybindings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -323,15 +323,17 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
 
       -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
+      -- vim.keymap.set('n', '<leader>/', function()
+      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+      -- builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+      --   winblend = 10,
+      --   previewer = false,
+      -- })
+      -- end, { desc = '[/] Fuzzily search in current buffer' })
+      vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
 
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sc', builtin.git_status, { desc = '[S]earch [C]hanged files' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = '[S]earch [G]it files' })
