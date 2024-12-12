c1eefe6d45aaac74eaf04bcaf2a43f04e94f070e
Yury Nekhaevskiy <nekhaevskiy@macbook-air>
2024-12-12 10:36:42 +0100

    nvim: update mini.statusline config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -809,14 +809,26 @@ require('lazy').setup({
       --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = true }
+      statusline.setup {
+        use_icons = true,
+      }
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_filename = function()
+        return '%f'
+      end
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      -- statusline.section_fileinfo = function()
+      --   return '%Y'
+      -- end
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return 'Ln %2l/%2L, Col %-2v'
       end
 
       -- ... and there is more!
