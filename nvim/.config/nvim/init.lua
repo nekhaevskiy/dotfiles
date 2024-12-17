452dc92a2a657bf9d8a57a18d81d328433d62075
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2024-12-17 20:45:23 +0100

    nvim: add typescript filetype to conform.nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -668,6 +668,7 @@ require('lazy').setup {
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
