67e67a4666068a8a2901b5a56d406fa00ff6308f
Yury Nekhaevskiy <nekhaevskiy@macbook-air>
2024-12-24 12:07:09 +0100

    remove unnecessary ArchLinux staff

--- screenlayout/.screenlayout/home.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --mode 1920x1200 --pos 3840x400 --rotate normal --output DP-1 --primary --mode 3840x1600 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off
