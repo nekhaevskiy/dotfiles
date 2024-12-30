a1adb632abc5821dea848f7213facf7185b5dd9b
Yury Nekhaevskiy <nekhaevskiy@macbook-air>
2024-12-30 08:36:35 +0100

    nvim: disable flash.nvim for f/F search

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,7 +1,13 @@
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
-  opts = {},
+  opts = {
+    modes = {
+      char = {
+        enabled = false,
+      },
+    },
+  },
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
