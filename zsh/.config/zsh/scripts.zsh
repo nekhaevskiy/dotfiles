74cab9c351a019b7c12115090d2837da7554128f
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2025-01-07 10:22:56 +0100

    zsh: add git-bc-cherry-pick-improved script

--- zsh/.config/zsh/scripts.zsh
+++ zsh/.config/zsh/scripts.zsh
@@ -1,4 +1,4 @@
-# save cluster IP
+# cluster IP save
 cips() {
   # Check if an argument is provided
   if [ -z "$1" ]; then
@@ -12,11 +12,21 @@ cips() {
   echo "Cluster IP saved."
 }
 
-# load cluster IP
+# cluster IP load
 cipl() {
   export CLUSTER_IP=$(cat "$HOME/work/cluster-ip")
   echo "$CLUSTER_IP" | xsel -ib
   echo "$CLUSTER_IP"
+}
+
+# git bc-cherry-pick improved
+gbcpi() {
+  if [ -z "$1" ]; then
+    echo "No branch provided."
+    return 1
+  fi
+
+  git bc-show-eligible "$1" | fzf --no-sort --reverse | grep -o '^[a-f0-9]\{40\}' | xargs git bc-cherry-pick
 }
 
 # remove a user-specified IP from the known_hosts file
