ebac6a48abc24e97c61e0a1ddf0d786707651558
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2025-01-14 11:14:37 +0100

    nvim: use sc as keymap for change surrounding

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -20,7 +20,11 @@ return { -- Collection of various small independent plugins/modules
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    require('mini.surround').setup {
+      mappings = {
+        replace = 'sc', -- Change surrounding
+      },
+    }
 
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
