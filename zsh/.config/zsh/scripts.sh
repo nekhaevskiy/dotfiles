93555bc0ec6e95809829402b54aae061ccb4bb45
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2025-02-28 13:11:55 +0100

    zsh: use only git diff in preview in _fzf_git_files

--- zsh/.config/zsh/scripts.sh
+++ zsh/.config/zsh/scripts.sh
@@ -1,3 +1,27 @@
+# _fzf_git_files without file preview
+_fzf_git_files() {
+    _fzf_git_check || return
+    local root query
+    root=$(git rev-parse --show-toplevel)
+    [[ $root != "$PWD" ]] && query='!../ '
+
+    (
+        git -c color.status=$(__fzf_git_color) status --short --no-branch
+        git ls-files "$root" | grep -vxFf <(
+            git status -s | grep '^[^?]' | cut -c4-
+            echo :
+        ) | sed 's/^/   /'
+    ) |
+        _fzf_git_fzf -m --ansi --nth 2..,.. \
+            --border-label '📁 Files ' \
+            --header 'CTRL-O (open in browser) ╱ ALT-E (open in editor)' \
+            --bind "ctrl-o:execute-silent:bash \"$__fzf_git\" --list file {-1}" \
+            --bind "alt-e:execute:${EDITOR:-vim} {-1} > /dev/tty" \
+            --query "$query" \
+            --preview "git diff --no-ext-diff --color=$(__fzf_git_color .) -- {-1} | $(__fzf_git_pager)" "$@" |
+        cut -c4- | sed 's/.* -> //'
+}
+
 # cluster IP save
 cips() {
     # Check if an argument is provided
@@ -229,9 +253,6 @@ gwa() {
 
     git worktree add --track -b "$branch" "$branch" "origin/$branch"
     cd "$branch"
-
-    # git fetch
-    # git branch --set-upstream-to origin/"$branch"
 }
 
 # git worktree add - create a new branch
