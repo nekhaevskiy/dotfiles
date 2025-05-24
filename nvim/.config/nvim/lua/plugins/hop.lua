76b8b1c7e9a0c0f5765863a6384e18d0c9290808
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2025-05-24 12:16:51 +0200

    nvim: disable hop and vim-sleuth plugins

--- nvim/.config/nvim/lua/plugins/hop.lua
+++ nvim/.config/nvim/lua/plugins/hop.lua
@@ -1,3 +1,7 @@
+if true then
+  return {}
+end
+
 return {
   'smoka7/hop.nvim',
   version = '*',
--- nvim/.config/nvim/lua/plugins/vim-sleuth.lua
+++ nvim/.config/nvim/lua/plugins/vim-sleuth.lua
@@ -1,3 +1,7 @@
+if true then
+  return {}
+end
+
 return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 }
