9ddf992cf47b466fee9a051b932c511c3fa5ac62
Yury Nekhaevskiy <nekhaevskiy@macbook-air>
2025-06-11 14:48:47 +0200

    zsh: add pyenv for macos

--- zsh/.config/zsh/macos.sh
+++ zsh/.config/zsh/macos.sh
@@ -19,6 +19,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
+
 # starship
 eval "$(starship init zsh)"
 
