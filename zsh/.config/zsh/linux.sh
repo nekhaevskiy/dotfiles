c2020158759836363f9890eff81366e83b8a1dab
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2025-06-12 20:11:56 +0200

    zsh: update initialization of pyenv and pyenv-virtualenv

--- zsh/.config/zsh/linux.sh
+++ zsh/.config/zsh/linux.sh
@@ -19,13 +19,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-# pyenv (needed for Jupyter)
+# pyenv (needed for JupyterLab)
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init - bash)"
-
-# pyenv-virtualenv (needed for Jupyter)
-eval "$(pyenv virtualenv-init -)"
+eval "$(pyenv init - zsh)"
 
 # starship
 eval "$(starship init zsh)"
