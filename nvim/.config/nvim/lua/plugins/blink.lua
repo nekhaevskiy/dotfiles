b907244a5e89198f0370ab71df9bd8121a89a75b
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2025-08-28 10:49:51 +0200

    nvim: update blink config

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,73 +1,31 @@
-_G.ai_enabled = true
-
-return { -- Autocompletion
+return {
   'saghen/blink.cmp',
-  version = '1.*',
   dependencies = {
-    -- GitHub Copilot
-    {
-      'giuxtaposition/blink-cmp-copilot',
-    },
-    -- Snippet Engine
-    -- {
-    --   'L3MON4D3/LuaSnip',
-    --   version = '2.*',
-    --   build = (function()
-    --     -- Build Step is needed for regex support in snippets.
-    --     -- This step is not supported in many windows environments.
-    --     -- Remove the below condition to re-enable on windows.
-    --     if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-    --       return
-    --     end
-    --     return 'make install_jsregexp'
-    --   end)(),
-    --   dependencies = {
-    --     -- `friendly-snippets` contains a variety of premade snippets.
-    --     --    See the README about individual language/framework/plugin snippets:
-    --     --    https://github.com/rafamadriz/friendly-snippets
-    --     -- {
-    --     --   'rafamadriz/friendly-snippets',
-    --     --   config = function()
-    --     --     require('luasnip.loaders.from_vscode').lazy_load()
-    --     --   end,
-    --     -- },
-    --   },
-    --   opts = {},
-    -- },
+    'giuxtaposition/blink-cmp-copilot',
     'folke/lazydev.nvim',
   },
+  version = '1.*',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
   opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
-      -- 'default' (recommended) for mappings similar to built-in completions
-      --   <c-y> to accept ([y]es) the completion.
-      --    This will auto-import if your LSP supports it.
-      --    This will expand snippets if the LSP sent a snippet.
-      -- 'super-tab' for tab to accept
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- For an understanding of why the 'default' preset is recommended,
-      -- you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      --
-      -- All presets have the following mappings:
-      -- <tab>/<s-tab>: move to right/left of your snippet expansion
-      -- <c-space>: Open menu or open docs if already open
-      -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-      -- <c-e>: Hide menu
-      -- <c-k>: Toggle signature help
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
       preset = 'default',
-
       ['<CR>'] = { 'accept', 'fallback' },
-
-      ['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
-      ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
-
-      -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-      --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      -- ['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
+      -- ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
     },
 
     appearance = {
@@ -76,54 +34,35 @@ return { -- Autocompletion
       nerd_font_variant = 'mono',
     },
 
-    completion = {
-      -- Disable auto brackets
-      -- NOTE: some LSPs may add auto brackets themselves anyway
-      accept = { auto_brackets = { enabled = true } },
-      -- By default, you may press `<c-space>` to show the documentation.
-      -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = true, auto_show_delay_ms = 500 },
-    },
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = false } },
 
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'lazydev', 'copilot' },
+      default = { 'lazydev', 'lsp', 'path', 'buffer', 'copilot' },
       providers = {
-        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        },
         copilot = {
           name = 'copilot',
           module = 'blink-cmp-copilot',
-          enabled = function()
-            return _G.ai_enabled
-          end,
-          async = true,
           score_offset = 100,
+          async = true,
         },
       },
     },
 
-    -- snippets = { preset = 'luasnip' },
-
-    -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-    -- which automatically downloads a prebuilt binary when enabled.
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
     --
-    -- By default, we use the Lua implementation instead, but you may enable
-    -- the rust implementation via `'prefer_rust_with_warning'`
-    --
-    -- See :h blink-cmp-config-fuzzy for more information
+    -- See the fuzzy documentation for more information
     fuzzy = { implementation = 'lua' },
-
-    -- Shows a signature help window while you type arguments for a function
-    signature = { enabled = false },
   },
-  keys = {
-    {
-      '<leader>ta',
-      function()
-        _G.ai_enabled = not _G.ai_enabled
-        print('Copilot is now ' .. (_G.ai_enabled and 'enabled' or 'disabled'))
-        require('blink-cmp').reload()
-      end,
-      desc = '[T]oggle [A]I',
-    },
-  },
+  opts_extend = { 'sources.default' },
 }
