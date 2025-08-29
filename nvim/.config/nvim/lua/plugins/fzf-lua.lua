0582ae70402c318992aee06e5f4d12732ea9a8d1
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2025-08-29 19:55:04 +0200

    nvim: update fzf-lua config

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -2,44 +2,113 @@ return {
   'ibhagwan/fzf-lua',
   -- optional for icon support
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    -- calling `setup` is optional for customization
-    require('fzf-lua').setup {
-      winopts = {
-        preview = {
-          layout = 'vertical',
-          vertical = 'down:70%',
-        },
-      },
-      keymap = {
-        builtin = {
-          true,
-          ['<S-down>'] = 'preview-down',
-          ['<S-up>'] = 'preview-up',
-          ['<M-S-down>'] = 'preview-page-down',
-          ['<M-S-up>'] = 'preview-page-up',
-        },
-        fzf = {
-          true,
-          ['shift-down'] = 'preview-down',
-          ['shift-up'] = 'preview-up',
-        },
-      },
-    }
-
-    vim.keymap.set('n', '<leader>/', require('fzf-lua').lgrep_curbuf, { desc = 'Find in current buffer ("/")' })
-    vim.keymap.set('n', '<leader>fb', require('fzf-lua').buffers, { desc = '[B]uffers' })
-    vim.keymap.set('n', '<leader>ff', require('fzf-lua').files, { desc = '[F]iles' })
-    vim.keymap.set('n', '<leader>fg', require('fzf-lua').git_status, { desc = 'Git [S]tatus' })
-    vim.keymap.set('n', '<leader>fG', require('fzf-lua').grep, { desc = '[G]rep' })
-    vim.keymap.set('n', '<leader>fh', require('fzf-lua').helptags, { desc = '[H]elp' })
-    vim.keymap.set('n', '<leader>fl', require('fzf-lua').live_grep, { desc = '[L]ive Grep' })
-    vim.keymap.set('n', '<leader>fo', require('fzf-lua').oldfiles, { desc = '[O]ld Files' })
-    vim.keymap.set('n', '<leader>fr', require('fzf-lua').registers, { desc = '[R]egisters' })
-    vim.keymap.set('n', '<leader>fR', require('fzf-lua').resume, { desc = '[R]esume' })
+  keys = {
+    {
+      '<leader>/',
+      function()
+        require('fzf-lua').lgrep_curbuf()
+      end,
+      desc = 'Find in current buffer ("/")',
+    },
+    {
+      '<leader>fb',
+      function()
+        require('fzf-lua').buffers()
+      end,
+      desc = '[b]uffers',
+    },
+    {
+      '<leader>ff',
+      function()
+        require('fzf-lua').files()
+      end,
+      desc = '[f]iles',
+    },
+    {
+      '<leader>fg',
+      function()
+        require('fzf-lua').git_status()
+      end,
+      desc = '[g]it status',
+    },
+    {
+      '<leader>fG',
+      function()
+        require('fzf-lua').grep()
+      end,
+      desc = '[G]rep',
+    },
+    {
+      '<leader>fh',
+      function()
+        require('fzf-lua').helptags()
+      end,
+      desc = '[h]elp',
+    },
+    {
+      '<leader>fl',
+      function()
+        require('fzf-lua').live_grep()
+      end,
+      desc = '[l]ive grep',
+    },
+    {
+      '<leader>fo',
+      function()
+        require('fzf-lua').oldfiles()
+      end,
+      desc = '[o]ld files',
+    },
+    {
+      '<leader>fr',
+      function()
+        require('fzf-lua').registers()
+      end,
+      desc = '[r]egisters',
+    },
+    {
+      '<leader>fR',
+      function()
+        require('fzf-lua').resume()
+      end,
+      desc = '[R]esume',
+    },
+    {
+      '<leader>dd',
+      function()
+        require('fzf-lua').diagnostics_document { sort = true }
+      end,
+      desc = '[d]iagnostics',
+    },
+    {
+      'grr',
+      function()
+        require('fzf-lua').lsp_references()
+      end,
+      desc = 'LSP References',
+    },
     -- vim.keymap.set('n', '<leader>fG', require('fzf-lua').git_files, { desc = '[G]it files' })
-    vim.keymap.set('n', '<leader>dd', function()
-      require('fzf-lua').diagnostics_document { sort = true }
-    end, { desc = '[D]iagnostics' })
-  end,
+  },
+  opts = {
+    winopts = {
+      preview = {
+        layout = 'vertical',
+        vertical = 'down:70%',
+      },
+    },
+    keymap = {
+      builtin = {
+        true,
+        ['<S-down>'] = 'preview-down',
+        ['<S-up>'] = 'preview-up',
+        ['<M-S-down>'] = 'preview-page-down',
+        ['<M-S-up>'] = 'preview-page-up',
+      },
+      fzf = {
+        true,
+        ['shift-down'] = 'preview-down',
+        ['shift-up'] = 'preview-up',
+      },
+    },
+  },
 }
