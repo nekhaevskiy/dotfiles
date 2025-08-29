914c4afa40464f476b7dee9943e6f580b3a811ba
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2025-08-29 20:00:42 +0200

    nvim: add separate mason-lspconfig

---
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,11 @@
+return {
+  'mason-org/mason-lspconfig.nvim',
+  dependencies = {
+    'mason-org/mason.nvim',
+    'neovim/nvim-lspconfig',
+    'j-hui/fidget.nvim',
+  },
+  opts = {
+    ensure_installed = { 'lua_ls', 'ts_ls' },
+  },
+}
