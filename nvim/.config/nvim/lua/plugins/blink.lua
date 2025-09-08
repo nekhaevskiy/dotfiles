22920ee9737e7feb48346c802f26edc97f524ab6
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2025-09-08 14:20:53 +0200

    nvim: update config for blink

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -33,8 +33,8 @@ return {
       nerd_font_variant = 'mono',
     },
 
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    -- Show the documentation popup automatically
+    completion = { documentation = { auto_show = true } },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
