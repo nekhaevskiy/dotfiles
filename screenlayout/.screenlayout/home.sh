726d2197e4dfea72340e55e18d53429d9a042bd2
Yury Nekhaevskiy <nekhaevskiy@dell-latitude-9440>
2025-09-09 10:36:14 +0200

    screenlayout: add layouts for different environments

---
+++ screenlayout/.screenlayout/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1200 --pos 3840x400 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 3840x1600 --pos 0x0 --rotate normal --output DP-5 --off
