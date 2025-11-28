895776888627e694574e9c567e7dcd561218124e
Yury Nekhaevskiy <ynekhaevskiy@dell.latitude>
2025-11-28 18:30:20 +0100

    nvim: disable hiding of backticks (```) in markdown

--- nvim-omarchy/.config/nvim/lua/config/autocmds.lua
+++ nvim-omarchy/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- disable the hiding (concealing) of backticks (```) in markdown
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
