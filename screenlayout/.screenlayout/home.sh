91068bf8b2a1f48e576400600c63336f4b67ccfa
Yury Nekhaevskiy <ynekhaevskiy@dell.latitude>
2026-01-14 12:40:16 +0100

    screenlayout: remove unused display layout scripts

--- screenlayout/.screenlayout/home.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1200 --pos 3840x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 3840x1600 --pos 0x0 --rotate normal --output DP-5 --off
