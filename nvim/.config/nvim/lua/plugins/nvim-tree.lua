695c7f4c259aa66044d638a60dce7a0edaa82b18
Yury Nekhaevskiy <ynekhaevskiy@dell.latitude>
2026-06-22 19:21:14 +0200

    nvim: replace nvim-tree with yazi as file manager

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-tree/nvim-tree.lua",
+  enabled = false,
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
     { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "File Manager" },
